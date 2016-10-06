3835e4fa1c0c1576d43d22633ca636a8b6942af7
godLYC <309080979@qq.com>
2016-10-06 23:01:01 +0800

    add perl file

---
+++ 111.pl
@@ -0,0 +1,65 @@
+#£¡usr/bin/perl
+
+#cd C:/Users/30908/Desktop
+
+
+
+#@lines = `perldoc -u -fatan2`;
+#foreach (@lines) {
+#	s/\w<(^>]+)>/\U$1/g;
+#	print;
+#}
+
+#my $a = !!'0';
+#print $a;
+
+
+#chomp($r = <STDIN>);
+#$zhouchang = $r*2*3.141592654;
+#print $zhouchang;
+
+
+#@rocks = qw/ bedrock slate lava/;
+#foreach $rock (@rocks) {
+#	$rock = "\t$rock";
+#	$rock .= "\n";
+#}
+#print "The rocks are:\n",@rocks;
+
+
+#foreach (1..10){
+#	print "I can count to $_!\n"
+#}
+
+
+#@lines = qw/ bedrock slate lava/;
+#print @lines;
+
+
+#/ifs/TJPROJ3/CR/Project/Life_business_sample
+
+
+
+
+'''
+#!/usr/bin/perl -w
+# Determining frequency of nucleotides
+# The DNA
+$DNA = ¡¯ACGGGAGGACGGGAAAATTACTACGGCATTAGC¡¯;
+#
+$a = 0; $c = 0; $g = 0; $t = 0; $e = 0;
+#
+while($DNA =~ m/a/ig){$a++}
+while($DNA =~ m/c/ig){$c++}
+while($DNA =~ m/g/ig){$g++}
+while($DNA =~ m/t/ig){$t++}
+while($DNA =~ m/[^acgt]/ig){$e++}
+#
+print "A=$a C=$c G=$g T=$t errors=$e\n";
+'''
+
+
+
+
+
+
